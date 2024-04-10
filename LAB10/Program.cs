a32da9bbe5b892d1c829675db1acad56a7378434
mav80 <mav80@ARTEMKA>
2024-04-10 23:24:44 +0700

    Добавьте файлы проекта.

---
+++ LAB10/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+
+abstract class Animal
+{
+    protected string name;
+
+    public Animal(string Name)
+    {
+        name = Name;
+    }
+
+    public abstract void Talk();
+    public virtual void PrintProperties()
+    {
+        Console.WriteLine("Имя: " + name);
+    }
+}
+
+abstract class Ungulate : Animal
+{
+    public Ungulate(string Name) : base(Name) { }
+    public abstract void EatGrass();
+}
+
+class Cow : Ungulate
+{
+    public Cow(string Name) : base(Name) { }
+
+    public override void Talk()
+    {
+        Console.WriteLine("Муу-у-у!");
+    }
+
+    public override void EatGrass()
+    {
+        Console.WriteLine(name + " ест траву.");
+    }
+}
+
+class Dog : Animal
+{
+    public Dog(string Name) : base(Name) { }
+
+    public override void Talk()
+    {
+        Console.WriteLine("Вуф!");
+    }
+}
+class Horse : Ungulate
+{
+    public Horse(string Name) : base(Name) { }
+
+    public override void Talk()
+    {
+        Console.WriteLine("И-го-го!");
+    }
+
+    public override void EatGrass()
+    {
+        Console.WriteLine(name + " ест траву.");
+    }
+}
+class Cat : Animal
+{
+    public Cat(string Name) : base(Name) { }
+
+    public override void Talk()
+    {
+        Console.WriteLine("Мяу!");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Animal> animals = new List<Animal> {
+            new Cow("Бесси"),
+            new Dog("Шарик"),
+            new Horse("Спирит"),
+            new Cat("Вискас")
+        };
+
+        foreach (var animal in animals)
+        {
+            animal.Talk();
+            animal.PrintProperties();
+            if (animal is Ungulate ungulate)
+            {
+                ungulate.EatGrass();
+            }
+            Console.WriteLine();
+        }
+    }
+}
